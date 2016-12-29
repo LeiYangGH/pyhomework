00b79bd09e9cfe30879917346110d7233aa20425
LeiYangGH <675686066@qq.com>
2016-12-29 16:43:39 +0800

    q3 can submit exam

--- zhaopeiyou/zhaopeiyou/zhaopeiyou.py
+++ zhaopeiyou/zhaopeiyou/zhaopeiyou.py
@@ -1,39 +1,44 @@
-sumabc = []
-for a in range(1,50):
-    for b in range(1,50):
-        for c in range(1,50):
-            if(a * b * c == 36):
-                #print(a,b,c)
-                abc = [a,b,c]#.sort()
-                abc.sort()
-                #sumabc.append([a+b+c,(a,b,c)])
-                sumabc.append([a+b+c,abc])
-#print(sumabc)
-d = dict()
-for k,x in sumabc:
-    if(k in d):
-        if(x not in d[k]):
-            d[k].append(x)
-    else:
-        d[k] = [x]
-#print(d)
-for k in d.keys():
-    print(k,d[k])
+#sumabc = []
+#for a in range(1,50):# assume child < 50
+#    for b in range(1,50):
+#        for c in range(1,50):
+#            if(a * b * c == 36):
+#                abc = [a,b,c]
+#                abc.sort()
+#                sumabc.append([a + b + c,abc])
+#d = dict()
+#for k,x in sumabc:
+#    if(k in d):
+#        if(x not in d[k]):
+#            d[k].append(x)
+#    else:
+#        d[k] = [x]
 
+#for k in d.keys():
+#    if(len(d[k]) > 1):
+#        print(k,d[k])
 
-#import sys
-#def calcs(t):
-#    #return t + 0.5 * 2*t*t
-#    return t + t*t
+ #in result, there's only one a,b,c
+ #that has at least 2 combinations
+
+import sys
+def calcs(t):
+    #formula: v0t + 1/2 a t^2
+    # v0 = 1, a = 2
+    return t + t * t
+
+inputstr = input('numbers seprated by space :')
+nums = inputstr.split(" ")
+#convert to int
+nums = [int(i) for i in nums]
+t = nums[0] # first number as T
+# remaining number as time
+for i in range(1,t + 1):
+    s = calcs(nums[i])
+    sys.stdout.write(str(s) + ' ')
 
 #inputstr = '2 1 2'#input('numbers seprated by space :')
-#nums = inputstr.split(" ")
-#nums = [int(i) for i in nums]
-#n = nums[0]
-#for i in range(1,n):
-#    s = calcs(nums[i])
-#    #print(str(s) + ' ')
-#    sys.stdout.write(str(s) + ' ')
+
 
 #inputstr = input('numbers seprated by , :')
 #nums = inputstr.split(",")
