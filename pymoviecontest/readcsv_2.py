adda2f803585cf033d1c1277fa98a8fac5c8a21a
LeiYangGH <675686066@qq.com>
2017-03-26 10:56:59 +0800

    readcsv 1-6

--- pymoviecontest/readcsv_2.py
+++ pymoviecontest/readcsv_2.py
@@ -7,8 +7,9 @@ import numpy as np
 import pandas as pd
 
 df20105=pd.read_csv('fbr_d_n_b.csv',names=['date','film','BoxOfficeReturn'])
-#print df20105
+print df20105
 df20105=df20105[df20105.BoxOfficeReturn.notnull()]
+print u'删除NaN的行---------------'
 print df20105
 
 val20105=df20105.values
@@ -19,18 +20,24 @@ for i in range(0,len(val20105)):
     lst00.append(lx[0]+lx[1]+lx[2])
 df20105.index= lst00
 df20105=df20105.drop(['date'],axis=0)
-
-
-
-print  type(df20105)
-
-df20105 = df20105.sort(['date'], ascending=[1])
+print u'更换索引日期格式---------------'
 print df20105
 
-#df20105=df20105.filter(date='bbi', axis=0)
+print u'输出数据类型---------------'
+print  type(df20105)
 
-##curdir = os.getcwd()
-##url = 'file:///' + os.path.join(curdir,u'www3\电影票房数据库.htm')
-##print url
+#df20105 = df20105.sort(['date'], ascending=[1])
+df20105 = df20105.sort_values(by='date')
+print u'按日期排序---------------'
+print df20105
+
+print u'票房大于1亿---------------'
+dfbig=df20105[(df20105['BoxOfficeReturn'] > 100000)]
+print dfbig
+
+dfrzdf=df20105[(df20105['film'] == r'让子弹飞')]
+print u'让子弹飞---------------'
+print dfrzdf
+
 
 print 'hello'
