19a6bc0d0a27b21e6e946a99e7ce36056cc6b9f0
LeiYangGH <675686066@qq.com>
2017-04-12 22:20:19 +0800

    remove empty words

--- PyReadWords/PyReadWords/PyReadWords.py
+++ PyReadWords/PyReadWords/PyReadWords.py
@@ -3,14 +3,16 @@ def get_words_from_file(filename="synthetic.txt"):
     alltxt = file.read()
     words = alltxt.split()
     stripwords = [w.strip(r'"-:\';,.') for w in words]
-    alphawords = [w for w in stripwords if all(c.isalpha() for c in w)]
+    nonemptywords = [w for w in stripwords if len(w) > 0]
+    alphawords = [w for w in nonemptywords if all(c.isalpha() for c in w)]
     lowerwords = [w.lower() for w in alphawords]
     return lowerwords
 
-filename = "synthetic.txt"
+#filename = "synthetic.txt"
+filename = "pg84.txt"
 words = get_words_from_file(filename)
 print(filename, "loaded ok.")
 print("{} valid words found.".format(len(words)))
 print("Valid word list:")
-for word in words:
-    print(word)
+#for word in words:
+#    print(word)
