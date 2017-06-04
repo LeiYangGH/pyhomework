a6ced3c113934e264d1ba75815aae6cc279117bc
LeiYangGH <675686066@qq.com>
2017-06-04 11:17:54 +0800

    add func defs

--- PyParallelogram/PyParallelogram/PyParallelogram.py
+++ PyParallelogram/PyParallelogram/PyParallelogram.py
@@ -1,12 +1,24 @@
 a1,a2,a3,b1,b2,b3,c1,c2,c3 = 2,3,4,5,6,4,7,8,9
-def inputabc():
+def read_input():
     a1,a2,a3,b1,b2,b3,c1,c2,c3 = input("please input a1 a2 a3 b1 b2 b3 c1 c2 c3:\n").split() 
-    #print(a1)
-    #print(c3)
+
+def calc_vector_magnitude(a1,a2,a3,b1,b2,b3):
+
+def calc_vector2_cross(a1,a2,a3,b1,b2,b3):
+    
+def calc_vector2_dotproduct(a1,a2,a3,b1,b2,b3):
+
+def calc_vector_magnitude(a1,a2,a3):
+
+def calc_area(a1,a2,a3,b1,b2,b3):
+
+def calc_volumn(a1,a2,a3,b1,b2,b3):
+
+def print_output(a1,a2,a3,b1,b2,b3,c1,c2,c3,area,volumn):
 
 if __name__ == "__main__":
-    print ('---Start---')
-    #inputabc()
+    print('---Start---')
+    #read_input()
     print(a1)
     print(c3)
-    print ('---End---')
+    print('---End---')
