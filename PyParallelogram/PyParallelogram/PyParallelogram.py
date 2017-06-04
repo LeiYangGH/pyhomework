16406b67ca667c00206f8288fade1e42f6b99603
LeiYangGH <675686066@qq.com>
2017-06-04 12:54:46 +0800

    parallelogram pay

--- PyParallelogram/PyParallelogram/PyParallelogram.py
+++ PyParallelogram/PyParallelogram/PyParallelogram.py
@@ -2,6 +2,7 @@ import math
 def read_input():
     #sample input:2 3 4 5 6 4 7 8 9
     a1,a2,a3,b1,b2,b3,c1,c2,c3 = map(float,input("please input a1 a2 a3 b1 b2 b3 c1 c2 c3:\n").split())
+    #a1,a2,a3,b1,b2,b3,c1,c2,c3 = map(float,(1,2,3,4,5,6,7,8,9))
     return (a1,a2,a3,b1,b2,b3,c1,c2,c3)
 
 def vector_magnitude(g1,g2,g3):
@@ -17,20 +18,20 @@ def dot_product(g1,g2,g3,h1,h2,h3):
     return g1 * h1 + g2 * h2 + g3 * h3
 
 def area_parallelogram(g1,g2,g3,h1,h2,h3):
-    k1,k2,k3 = cross_product(g1,g2,g3,h1,h2,h3)#?
+    k1,k2,k3 = cross_product(g1,g2,g3,h1,h2,h3)
     return vector_magnitude(k1,k2,k3)
 
 def volumn_parallelepiped(g1,g2,g3,h1,h2,h3,k1,k2,k3):
-    k1,k2,k3 = cross_product(h1,h2,h3,k1,k2,k3)#?
-    dot = dot_product(g1,g2,g3,k1,k2,k3)
+    x,y,z = cross_product(h1,h2,h3,k1,k2,k3)
+    dot = dot_product(g1,g2,g3,x,y,z)
     return abs(dot)
 
 def print_output(a1,a2,a3,b1,b2,b3,c1,c2,c3,area,volumn):
     print('vector a=({0},{1},{2})'.format(a1,a2,a3))
     print('vector b=({0},{1},{2})'.format(b1,b2,b3))
     print('vector c=({0},{1},{2})'.format(c1,c2,c3))
-    print('area = {0}'.format(c1,c2,c3))
-    print('volumn = {0}'.format(c1,c2,c3))
+    print('area = {0}'.format(area))
+    print('volumn = {0}'.format(volumn))
 
 if __name__ == "__main__":
     print('---Start---')
