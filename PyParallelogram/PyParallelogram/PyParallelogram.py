5a70591a35dd346993053245a042c5f22b9eefe7
LeiYangGH <675686066@qq.com>
2017-06-04 11:08:31 +0800

    main

--- PyParallelogram/PyParallelogram/PyParallelogram.py
+++ PyParallelogram/PyParallelogram/PyParallelogram.py
@@ -1,3 +1,12 @@
-a1,a2,a3,b1,b2,b3,c1,c2,c3 = input("please input a1 a2 a3 b1 b2 b3 c1 c2 c3:\n").split() 
-print(a1)
-print(c3)
+a1,a2,a3,b1,b2,b3,c1,c2,c3 = 2,3,4,5,6,4,7,8,9
+def inputabc():
+    a1,a2,a3,b1,b2,b3,c1,c2,c3 = input("please input a1 a2 a3 b1 b2 b3 c1 c2 c3:\n").split() 
+    #print(a1)
+    #print(c3)
+
+if __name__ == "__main__":
+    print ('---Start---')
+    #inputabc()
+    print(a1)
+    print(c3)
+    print ('---End---')
