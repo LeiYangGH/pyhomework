6c6485adead9dce9534cf64450148e864ce01539
LeiYangGH <675686066@qq.com>
2017-08-28 10:49:42 +0800

    sinapic browser as parameter

--- selenium/sinapic/sinapic/sinapic.py
+++ selenium/sinapic/sinapic/sinapic.py
@@ -3,34 +3,39 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
+#driver = None
+def sina_pic_tabs(browser='Chrome'):
+    if browser == 'Chrome':
+        driver = webdriver.Chrome(r'C:\Installs\chromedriver.exe')
+        print("Chrome")
+    else:
+        driver = webdriver.Firefox(executable_path=r'C:\Installs\geckodriver.exe')
+        print("Firefox")
+    
 
-#driver = webdriver.Chrome(r'C:\Installs\chromedriver.exe')
-driver = webdriver.Firefox()
-print("Chrome")
+    driver.get(r'http://photo.sina.com.cn/')
+    if browser == 'Chrome':
+        driver.maximize_window()
+    else:
+        driver.set_window_size("1024", "768")
 
-driver.get(r'http://photo.sina.com.cn/')
-#driver.maximize_window()
-dirver.set_window_size("1024", "768")
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight/2);")
-div = driver.find_element_by_class_name("column-subshow-tab")
-#print(type(div))
-#print(type(div.text))
-print(div.text)
-#print(div.get_attribute('innerHTML'))
-div.location_once_scrolled_into_view
-print("scrolled")
-#subs = div.find_elements_by_xpath(".//*")
-subs = div.find_elements_by_xpath(".//ul/*")
-#sub0 = div.findElement(By.xpath("./ul/li"));
-#[print(sb.text) for sb in subs]
-for sb in subs:
-    if len(sb.text) >= 2:
-        sb.click()
-        time.sleep(1)
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight/2);")
+    div = driver.find_element_by_class_name("column-subshow-tab")
+    print(div.text)
+    div.location_once_scrolled_into_view
+    print("scrolled")
+    subs = div.find_elements_by_xpath(".//ul/*")
+    for sb in subs:
+        if len(sb.text) >= 2:
+            sb.click()
+            time.sleep(1)
+    input("exit?")
+    driver.quit()
  
+sina_pic_tabs()
+#sina_pic_tabs("Firefox")
 
-input("exit?")
-driver.quit()
 
 
 
